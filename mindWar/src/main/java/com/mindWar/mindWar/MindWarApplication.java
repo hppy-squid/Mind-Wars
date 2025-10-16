2ebe0294b3ab7e4a66dd3a6620ebe2a366da1602
Felicia Karlsson <felizia02@icloud.com>
2025-10-16 16:30:28 +0200

    springboot projekt skapat

---
+++ mindWar/src/main/java/com/mindWar/mindWar/MindWarApplication.java
@@ -0,0 +1,13 @@
+package com.mindWar.mindWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindWarApplication.class, args);
+	}
+
+}
---
+++ mindWar/src/test/java/com/mindWar/mindWar/MindWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindWar.mindWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
