5ab50a5a95b093a507ff17108a194ea566cd8bd2
Felicia Karlsson <felizia02@icloud.com>
2025-10-29 09:51:49 +0100

    gåtan skickas via websocket

--- mindWar/src/main/java/com/mindWar/mindWar/websocket/controller/StompController.java
+++ mindWar/src/main/java/com/mindWar/mindWar/websocket/controller/StompController.java
@@ -1,10 +1,16 @@
 package com.mindWar.mindWar.websocket.controller;
 
+import java.util.Map;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import com.mindWar.mindWar.openAi.model.Riddle;
+import com.mindWar.mindWar.openAi.service.RiddleService;
 import com.mindWar.mindWar.websocket.model.Chat;
 import com.mindWar.mindWar.websocket.model.ChatMessage;
 import com.mindWar.mindWar.websocket.model.Hello;
@@ -13,6 +19,14 @@ import com.mindWar.mindWar.websocket.model.HelloMessage;
 @Controller
 @CrossOrigin(origins = "*")
 public class StompController {
+
+        private final SimpMessagingTemplate messagingTemplate;
+    private final RiddleService riddleService;
+
+    public StompController(SimpMessagingTemplate messagingTemplate, RiddleService riddleService) {
+        this.messagingTemplate = messagingTemplate;
+        this.riddleService = riddleService;
+    }
 
         @MessageMapping("/hello")
     @SendTo("/topic/greetings")
@@ -27,5 +41,19 @@ public class StompController {
         System.out.println("chat");
         return new Chat(chat.getName() + ": " + chat.getContent());
 }
+
+ @MessageMapping("/riddle/next")
+    public void nextRiddle() {
+        Riddle newRiddle = riddleService.generateRiddle();
+
+        // skicka ut till alla spelare via topic
+        messagingTemplate.convertAndSend("/topic/riddle", newRiddle);
+    }
+
+    @MessageMapping("/riddle/correct")
+    public void correctGuess(@Payload String playerName) {
+        messagingTemplate.convertAndSend("/topic/riddle",
+            Map.of("type", "CORRECT_GUESS", "player", playerName));
+    }
     
 }
